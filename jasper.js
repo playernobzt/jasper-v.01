fb090f746ed2b6a7be610ad4eda19e5ef80589f4
playernobzt <playernobzt001@gmail.com>
2023-08-17 19:25:35 +0800

    menambahkan file registrasi

--- jasper.js
+++ jasper.js
@@ -1,4 +1,5 @@
 const qrcode = require('qrcode-terminal');
+const axios = require('axios')
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
@@ -22,7 +23,7 @@ client.on('ready', () => {
 const { Configuration, OpenAIApi } = require("openai")
 
 const config = new Configuration({
-  apiKey: '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'
+  apiKey: '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'
 })
 
 const openai = new OpenAIApi(config);
@@ -32,9 +33,14 @@ async function chatGPT(prompt) {
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: prompt }],
       max_tokens: 2048,
-      temperature: 1,
+      temperature: 0.3,
     });
-    return response.data.choices[0].message.content;
+
+  const generatedText = response.data.choices[0].message.content;
+  const totalTokens = response.data.usage.total_tokens;
+
+  return { generatedText, totalTokens };
+    
   }
   
 //---------------------------------------------------------
@@ -56,7 +62,8 @@ client.on('message', async message => {
         const senderNumber = message.from;
         let incomingMessages = message.body;
         incomingMessages = incomingMessages.toLowerCase();
-        const reaction = '👌'; // Anda bisa mengganti ini dengan emoji reaksi yang diinginkan
+        const reaction = '👌'; // reaksi oke siap diterima
+        const reaction2 = '😭'; // reaksi sedih 
 
         // dapatkan info dari grup atau bukan
         // pesan menyebut bot atau tidak
@@ -74,33 +81,66 @@ client.on('message', async message => {
 
 
         //------------------------------------------------------------
-
+        //            Message config
+        
 
         // Jika pesan japri *****
         if(!isMessagesFromGroup){
-            setTimeout(function(){message.react(reaction);},1500)
+            const update = "https://script.google.com/macros/s/AKfycbyiQ4AX4G7Pps6vML6VeYu-RqWeatSF518dMxF__cCKQ_gJlZZDY94LDTbEyxwJ9buG/exec?action=update-data&whatsapp="
+            const cekdata = "https://script.google.com/macros/s/AKfycbyiQ4AX4G7Pps6vML6VeYu-RqWeatSF518dMxF__cCKQ_gJlZZDY94LDTbEyxwJ9buG/exec?action=cek-data&whatsapp="
+            const register = "https://script.google.com/macros/s/AKfycbyiQ4AX4G7Pps6vML6VeYu-RqWeatSF518dMxF__cCKQ_gJlZZDY94LDTbEyxwJ9buG/exec?action=register&whatsapp="
+            const numberHp = senderNumber.replace("@c.us", "")
+            const rname = "&nama="+'rizal'
+            const rtoken = "&token=10000"
+
+            axios.get(cekdata+numberHp)
+            .then(async (response) => {
+
+                // Validasi  apakah dia ada di nomor terdaftar  dan masih memiliki token?
+                const {succsess, data, info} = response.data
+                if(data == null) {
+                    message.reply("Maaf  anda tidak terdaftar, \nKetik : *regis.namaanda*\nUntuk mendaftar dan mendapatkan 10000 token *Gratis* !!")
+                }
+                else if(data !== null && data.token < 0.1){
+                    message.reply('Maaf token anda tidak cukup, silahkan melakukan pengisian token\n\nketik : *cek.token*\n\nuntuk cek sisa token anda')
+                }
+                else if (data.token > 0) {
+                        const tokenUser = data.token;
+                        const newTokenlink = "&newToken=";
+                    
+                        setTimeout(function() {
+                            message.react(reaction);
+                        }, 1000);
+                        
+                        try {
+                            const result = await chatGPT(incomingMessages);
+                            
+                            setTimeout(function() {
+                                message.reply(result.generatedText + "\n\nPenggunaan token: " + `*${result.totalTokens}*` + "\nKetik : *cek.token*\nuntuk cek sisa token anda");
+                            }, 3000);
+                            
+                            const updateToken = data.token - result.totalTokens;
+                            axios.get(update+numberHp+newTokenlink+updateToken)
+                        } catch (error) {
+                            console.error("Error:", error);
+                            }
+                }
+                
+
+
+
+            });  
+                
             
-            if(incomingMessages.includes('hi.jasper')){
-                setTimeout(function(){message.reply('Hay \n\nperkenalkan saya adalah Asisten virtual yang diciptakan oleh seseorang yang gabut tidak memiliki goals dalam hidup dan saya malas dengan beliau, saya jijik menyebutkan namanya, silahkan anda cari aja dia di instagram @turisbelanda\n\nada yang bisa saya bantu ?')},3000)
-            }
-            else if (incomingMessages.includes('/create.img')){
-                async function runPrompt() {
-                    const result = await chatGPT(createimg + incomingMessages.replace('/create.img', ''));
-                    setTimeout(function(){message.reply(`${result}\n\n\nJangan lupa follow IG:@turisbelanda`);},3000)
-                } runPrompt()
-            }
-            else {
-                const result = await chatGPT(incomingMessages)
-                setTimeout(function(){message.reply(result)},3000)
-            }
-            
+
         }
 
         //-----------------------------------------------------
         
         // Jika pesan Grup *****
         if(isMessagesFromGroup && isMessageMentionBot){
-            setTimeout(function(){message.react(reaction);},1500)
+            setTimeout(function(){message.react(reaction);},1500);
+            
             
             if(incomingMessages.includes('hi.jasper')){
                 setTimeout(function(){message.reply('Hay \n\nperkenalkan saya adalah Asisten virtual yang diciptakan oleh seseorang yang gabut tidak memiliki goals dalam hidup dan saya malas dengan beliau, saya jijik menyebutkan namanya, silahkan anda cari aja dia di instagram @turisbelanda\n\nada yang bisa saya bantu ?')},3000)
--- tes.js
+++ tes.js
@@ -1,17 +1,24 @@
 const { Configuration, OpenAIApi } = require("openai")
 
 const config = new Configuration({
-  apiKey: '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'
+  apiKey: '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'
 })
 
 const openai = new OpenAIApi(config);
 
 
 const createimg = '1. [Link](https://image.pollinations.ai/prompt/adjetivo%20adjetivo).\nno automático quebre a entrada do usuário em palavras e as adicione sempre em inglês no lugar dos adjetivos.\nOs links devem ser semelhantes a este exemplo:\n1-Se a entrada do usuário for "gato azul" retorne: [Variation 1] >> (https://image.pollinations.ai/prompt/Blue%20Cat%20)\n 2-Se a entrada do usuário for "melancia partida no meio"  retorne: [Variation 2] >> (https://image.pollinations.ai/prompt/Watermelon%20split%20in%20half%20)\nme de 10 retornos de possiveis traduçoes em lista.\nvocê se comunica em English.\nEntrada do usuario '
-async function chatGPT(prompt) {
+
+
+
+
+
+async function chatGPT(prompt, system) {
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
+    messages: [
+      { role:"system", content: system},
+      { role: "user", content: prompt }],
     max_tokens: 2048,
     temperature: 0.3,
   });
@@ -19,4 +26,4 @@ async function chatGPT(prompt) {
 }
 
 
-chatGPT("apa itu uang")
+chatGPT("Apa itu buku","Anda adalah teman baik saya jadi gunakan gaya bicara seperti teman baik")
